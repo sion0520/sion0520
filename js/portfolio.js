4349ace379297e61fe958dfaa3b7c5fd44a1bc16
sion0520 <133757349+sion0520@users.noreply.github.com>
2023-07-13 15:17:02 +0900

    add
    add

--- js/portfolio.js
+++ js/portfolio.js
@@ -2,8 +2,23 @@ $(function(){
     $('#fullpage').fullpage({
 		//options here
 		autoScrolling:true,
-		scrollHorizontally: true
+		scrollHorizontally: true,
+		KeyboardScrolling: true,
+		navigation : true
 	});
+
+
+	// 스크롤이동
+	$('.gnb li').click(function(){
+		let num = $(this).index()
+		console.log(num)
+
+		if(num==0){
+			$('body,html').animate({'scrollTop':0},800)
+		}else if(num==1){
+			$('body,html').animate({'scrollTop':1080},800)
+		}
+	})
 
     
 })
