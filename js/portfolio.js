70670a697ac18a63b3304c860850fd4b4ef06e98
sion0520 <133757349+sion0520@users.noreply.github.com>
2023-07-17 02:08:13 +0900

    add
    add

--- js/portfolio.js
+++ js/portfolio.js
@@ -9,33 +9,55 @@ $(function() {
 	});
 
 
-	//tab
+	
+	$('.slider_u').hide()
+	//탭메뉴
 	$('.tab li').click(function(){
 		$('.tab li').removeClass('on')
 		$(this).addClass('on')
+		$('.slider'). resize();
+		$('.slider'). slick('refresh');
+
+		let filter = $(this).index()
+		console.log(filter)
+
+		if(filter==0){
+			$('.slider_u, .slider_d').hide()
+			$('.slider_p').show()
+		}else if(filter==1){
+			$('.slider_p, .slider_d').hide()
+			$('.slider_u').show()
+		}else if(filter==2){
+			$('.slider_p, .slider_u, .slider_d').hide()
+		}else if(filter==3){
+			$('.slider_p, .slider_u').hide()
+			$('.slider_d').show()
+		}
 	})
 	
 
-	// 헤더 스크롤
-	$('.gnb li').click(function(){
-		let num = $(this).index()
-		console.log(num)
+	
 
-		if(num==0){
-			$('body,html').animate({'scrollTop':0},800)
-		}
-	})
+	// 헤더 스크롤
+	// $('.gnb li').click(function(){
+	// 	let num = $(this).index()
+	// 	console.log(num)
+
+	// 	if(num==0){
+	// 		$('body,html').animate({'scrollTop':0},800)
+	// 	}
+	// })
 
 	// 슬라이드
 	$('.slider').slick({
-        // 슬라이드 옵션 설정
         slidesToShow: 3,
         slidesToScroll: 1,
         autoplay: true,
         autoplaySpeed: 3000,
 		dots:true,
 		centerMode: true,
-		centerPadding: '60px',
+		setPosition:0
+		// centerPadding: '60px',
     });
 
 	
