3078f5c50fc50e5ab6b8796258304d82349a8311
sion0520 <133757349+sion0520@users.noreply.github.com>
2023-07-27 18:20:32 +0900

    add
    add

--- js/portfolio2.js
+++ js/portfolio2.js
@@ -12,7 +12,7 @@ $(function(){
     });
 
     //슬라이드 숨기기
-	$('.slider_u, .slider_d, .pront').hide()
+	$('.slider_u, .slider_d, .front').hide()
 	//탭메뉴
 	$('.tab li').click(function(){
 		$('.tab li').removeClass('on')
@@ -24,17 +24,16 @@ $(function(){
 		console.log(filter)
 
 		if(filter==0){
-			$('.slider_u, .slider_d, .slider_pront, .pront').hide()
+			$('.slider_u, .slider_d, .front').hide()
 			$('.slider_p').show()
 		}else if(filter==1){
-			$('.slider_p, .slider_d, .slider_pront, .pront').hide()
+			$('.slider_p, .slider_d, .front').hide()
 			$('.slider_u').show()
 		}else if(filter==2){
 			$('.slider_p, .slider_u, .slider_d').hide()
-            // $('.slider_pront').show()
-			$('.pront').show()
+			$('.front').show()
 		}else if(filter==3){
-			$('.slider_p, .slider_u, .slider_pront, .pront').hide()
+			$('.slider_p, .slider_u, .front').hide()
 			$('.slider_d').show()
 		}
 	})
